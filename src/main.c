c11436d977611581908f969140f289d151d53886
Cem Özden <cemozden.tr@gmail.com>
2022-06-27 23:19:44 +0200

    Adding ROM path

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+void print_help();
+
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    if (argc < 2) {
+        print_help();
+        return -1;
+    }
+
+    const char* rom_path = argv[1];
+
+    printf("ROM Path: %s\n", rom_path);
+
     return 0;
+}
+
+void print_help() {
+
+    printf("Usage: nesforfun ROM_FILE_PATH");
+
 }
